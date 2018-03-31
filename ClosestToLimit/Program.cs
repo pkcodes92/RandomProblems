1cfb24169228e522d7ef6b4a9d19a1d9affdaf88
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-03-31 15:19:34 -0400

    Many changes being made

--- ClosestToLimit/Program.cs
+++ ClosestToLimit/Program.cs
@@ -29,13 +29,32 @@ namespace ClosestToLimit
             int val2 = CalcDifference(y, limit);
 
             int finalResult = FindResult(val1, val2, x, y);
+
+            Console.WriteLine("Closest to {0} is {1}", limit, finalResult);
+
+            Console.ReadKey();
         }
 
+        /// <summary>
+        /// This is to find the difference between two numbers
+        /// </summary>
+        /// <param name="x">User provided input for the first number</param>
+        /// <param name="limit">User provided input for the limit integer</param>
+        /// <returns>A difference of limit from the variable x</returns>
         static int CalcDifference(int x, int limit)
         {
             return Math.Abs(x - limit); 
         }
 
+        /// <summary>
+        /// Finding which number was closest to the limit that was provided by the
+        /// user at the time that this console application runs
+        /// </summary>
+        /// <param name="val1">Result between the first integer and the limit</param>
+        /// <param name="val2">Result between the second integer and the limit</param>
+        /// <param name="x">The first integer provided by the user</param>
+        /// <param name="y">The second integer provided by the user</param>
+        /// <returns>An integer which is the number that is closest to the limit</returns>
         static int FindResult(int val1, int val2, int x, int y)
         {
             if (val1 == val2)
