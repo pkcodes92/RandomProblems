8dfbe2c0a5d2879c8f2ed58eb63451521f01704c
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-04-06 20:29:06 -0400

    Plenty of changes
    Also having the HackerRank problem added

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Date : 6th April 2018
+ * 
+ * Purpose : To solve the mini-max sum from HackerRank
+ *           in the language of C#
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            FindMiniMaxSum(arr);
+
+            Console.ReadKey();
+        }
+
+        static void FindMiniMaxSum(int[] arr)
+        {
+            long[] a = new long[5];
+            long sum = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] += (long)arr[i];
+                sum += a[i];
+            }
+
+            Array.Sort(a);
+            Console.WriteLine((sum - a[a.Length - 1]) + " " + (sum - a[0]));
+        }
+    }
+}
