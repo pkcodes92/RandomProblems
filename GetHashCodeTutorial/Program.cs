eb4c16fd0c18de3bd2ede65ad5ad92a77147eacd
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-04-26 15:11:26 -0400

    Committing the new project
    GetHashCode tutorial in progress

---
+++ GetHashCodeTutorial/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace GetHashCodeTutorial
+{
+    public struct Number
+    {
+        private int n; 
+
+        public Number(int value)
+        {
+            n = value;
+        }
+
+        public int Value
+        {
+            get { return n; }
+        }
+
+        public override bool Equals(Object obj)
+        {
+            if (obj == null || !(obj is Number))
+            {
+                return false;
+            }
+            else
+            {
+                return n == ((Number)obj).n;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return n; 
+        }
+
+        public override string ToString()
+        {
+            return n.ToString();
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random(); 
+        }
+    }
+}
