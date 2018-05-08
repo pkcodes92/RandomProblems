c3f824f05d5d10a582b1722351d4983103f2f126
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-05-08 12:47:08 -0400

    Various changes and adding more problems

---
+++ FirstNotRepeatingCharacter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FirstNotRepeatingCharacter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string of characters: ");
+            string s = Console.ReadLine();
+
+            char result = firstNonRepeatingCharacter(s);
+
+            Console.WriteLine($"Result: {result}");
+
+            Console.ReadKey(); 
+        }
+
+        static char firstNonRepeatingCharacter(string s)
+        {
+            char[] inputs = s.ToCharArray();
+
+            char result = '_';
+
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                if (s.LastIndexOf(s[i]) == s.IndexOf(s[i]))
+                {
+                    result = s[i];
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
