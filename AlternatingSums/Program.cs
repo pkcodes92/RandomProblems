3c1d9b7145a5e903b6ca835d1748cbf3eaf10ff1
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-05-09 20:20:17 -0400

    All of the code is updated
    Also adding the AlternatingSums problem

---
+++ AlternatingSums/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AlternatingSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[] { 40, 50, 60, 80, 10};
+
+            int[] weights = FindAltSums(a);
+
+            for (int i = 0; i < weights.Length; i++)
+            {
+                Console.Write(weights[i] + " ");
+            }
+
+            Console.ReadKey(); 
+        }
+
+        static int[] FindAltSums(int[] a)
+        {
+            int[] weights = new int[2];
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    weights[0] += a[i];
+                }
+
+                if (i % 2 == 1)
+                {
+                    weights[1] += a[i];
+                }
+            }
+
+            return weights;
+        }
+    }
+}
