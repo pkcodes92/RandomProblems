a846fac0395f18422ef82f97efe05ef83fbb86a7
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-05-21 17:37:18 -0400

    Having two new problems being added
    One for the even numbers only and another for a Company Challenge

---
+++ CompanyBotStrategy/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace CompanyBotStrategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] trainingData = new int[4][] { new int[] {3, 1},
+                                                  new int[] {6, 1},
+                                                  new int[] {4, 1},
+                                                  new int[] {5, 1} };
+
+            double answerTime = BotStrategy(trainingData);
+
+            Console.WriteLine($"The answer time is: {answerTime}");
+
+            Console.ReadKey(); 
+        }
+
+        static double BotStrategy(int[][] trainingData)
+        {
+            int trainingSum = 0, trainingCount = 0;
+            double average;
+
+            for (int i = 0; i < trainingData.Length; i++)
+            {
+                if (trainingData[i][1] == 1)
+                {
+                    trainingCount++;
+                    trainingSum += trainingData[i][0]; 
+                }
+            }
+
+            if (trainingCount > 0 && trainingSum > 0)
+            {
+                average = (double)trainingSum / trainingCount; 
+            }
+            else
+            {
+                average = 0; 
+            }
+
+            return average;
+        }
+    }
+}
---
+++ OnlyEvenNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OnlyEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 248622;
+
+            bool result = EvenDigitsOnly(n);
+
+            Console.WriteLine($"Does {n} contain only even digits? {result}");
+
+            Console.ReadKey();
+        }
+
+        static bool EvenDigitsOnly(int n)
+        {
+            var finalRes = true; 
+            int temp = n;
+            while (temp > 0)
+            {
+                if (temp % 2 != 0)
+                {
+                    finalRes = false;
+                }
+
+                temp = temp / 10; 
+            }
+
+            return finalRes; 
+        }
+    }
+}
