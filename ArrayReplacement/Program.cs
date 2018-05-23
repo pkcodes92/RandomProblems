9285e7906369159e5acbffaf47d8b87f98d9f36b
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-05-23 19:06:38 -0400

    Adding additional problems
    Having all of the code is updated

---
+++ ArrayReplacement/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ArrayReplacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 1, 2, 1 };
+
+            int elemToReplace = 1;
+
+            int substitutionElem = 3;
+
+            int[] finalArray = ArrayReplace(inputArray, elemToReplace, substitutionElem);
+
+            for (int i = 0; i < finalArray.Length; i++)
+            {
+                Console.Write(finalArray[i] + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int[] ArrayReplace(int[] inputArray, int elemToReplace, int substitutionElem)
+        {
+            int[] temp = new int[inputArray.Length];
+
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] == elemToReplace)
+                {
+                    temp[i] += substitutionElem;
+                }
+                else
+                {
+                    temp[i] += inputArray[i];
+                }
+            }
+
+            return temp;
+        }
+    }
+}
