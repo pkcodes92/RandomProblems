08e10a6349f2f6de6b04a75e4ff320deaa1c48ea
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-08-11 23:39:28 -0400

    Having a lot of the changes made
    Adding the ExtractEachKth element problem from CodeSignal added to the solution

---
+++ ExtractElements/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Problem: Given array of integers, remove each kth element from it.
+ */
+
+using System;
+using System.Linq;
+
+namespace ExtractElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Standard test case
+            int[] inputArray = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int k = 3;
+
+            int[] outputArray = ExtractEachKth(inputArray, k);
+            foreach (int item in outputArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            Console.ReadKey();
+        }
+
+        static int[] ExtractEachKth(int[] inputArray, int k)
+        {
+            return inputArray.Where((x, y) => y % k != k - 1).ToArray();
+        }
+    }
+}
