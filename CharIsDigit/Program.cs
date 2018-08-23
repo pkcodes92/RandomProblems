0b8a059f35d79682de41180fc665f1df794ff3da
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-08-22 21:38:13 -0400

    All of the code changes being made
    Adding more problem solutions to the repository and making sure all of the code properly compiles

---
+++ CharIsDigit/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CharIsDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an input to find out if it is a number: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+
+            bool finalResult = IsDigit(symbol);
+
+            Console.WriteLine($"Is {symbol} a digit? {finalResult}");
+
+            Console.ReadKey();
+        }
+
+        static bool IsDigit(char symbol)
+        {
+            bool valid = true;
+            if (char.IsNumber(symbol))
+            {
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+            }
+
+            return valid;
+        }
+    }
+}
---
+++ ElectionWinners/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ElectionWinners
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] votesIn = new int[] { 2, 3, 5, 2 };
+            int k = 2;
+
+            int result = ElectionsWinners(votesIn, k);
+
+            Console.WriteLine($"The final result: {result}");
+
+            Console.ReadKey(); 
+        }
+
+        static int ElectionsWinners(int[] votes, int k)
+        {
+            int max = votes.Max();
+            if (k == 0)
+            {
+                return votes.Count(x => x == max) > 1 ? 0 : 1;
+            }
+
+            return votes.Count(x => x + k > max);
+        }
+    }
+}
