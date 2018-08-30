1ff4d59f23160acaa7dcf8e92de2c94362e60289
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-08-30 12:31:38 -0400

    Having all of the latest problems added
    Adding more random problems that are being solved

---
+++ DigitRemoval/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DigitRemoval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = 152;
+            int maxValue = DeleteDigit(n);
+
+            Console.WriteLine($"Input value is: {n}");
+            Console.WriteLine($"Value is {maxValue}");
+            Console.ReadKey();
+        }
+
+        static int DeleteDigit(int n)
+        {
+            string s = n.ToString();
+            int i; 
+            for (i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] < s[i + 1])
+                    break;
+            }
+
+            return Convert.ToInt32(s.Remove(i, 1));
+        }
+    }
+}
---
+++ ReturnLongestWord/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace ReturnLongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = "Ready, steady, go!";
+            string output = LongestWord(text);
+
+            Console.WriteLine($"The longest word is: {output}");
+            Console.ReadKey();
+        }
+
+        static string LongestWord(string text)
+        {
+            string[] s = text.Split(new char[]
+            {
+                ']',
+                '[',
+                ',',
+                '!',
+                ' '
+            }, StringSplitOptions.RemoveEmptyEntries);
+
+            return s.OrderByDescending(x => x.Length).First();
+        }
+    }
+}
