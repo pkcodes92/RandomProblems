9f589427c0c530677b26a5252dd4a49237b6b022
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-10-22 22:46:41 -0400

    All of the latest changes being made - adding a GeeksForGeeks C# problem

---
+++ FindTheHiddenNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FindTheHiddenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a_temp = Console.ReadLine().Split(' ');
+            int[] a = Array.ConvertAll(a_temp, Int32.Parse);
+
+            int n = a.Length;
+
+            int hiddenNumber = FindHiddenNumber(a, n);
+
+            Console.WriteLine($"The hidden number in the array: {hiddenNumber}");
+
+            Console.ReadKey();
+        }
+
+        static int FindHiddenNumber(int[] a, int n)
+        {
+            long sum = 0; 
+            for(int i = 0; i < n; i++)
+            {
+                sum += a[i];
+            }
+
+            // Dividing the sum by size of n
+            long x = sum / n;
+
+            // Print x, if found
+            if (x * n == sum)
+            {
+                return (int)x; 
+            }
+            else
+            {
+                return -1; 
+            }
+        }
+    }
+}
