83d360ab3ef0485394ac5f4ae127c6ac0f2f6543
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-10-31 16:35:12 -0400

    Adding the largest number possible problem from HackerRank

---
+++ LargestNumberPossible/Program.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace LargestNumberPossible
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value for n to find the largest possible number that is n-digits in length: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            var largestNumber = FindLargestNumber(n);
+
+            Console.WriteLine($"Given the value of {n}, the largest number possible is {largestNumber}");
+
+            Console.ReadKey(); 
+        }
+
+        /// <summary>
+        /// Ability to find the largest number
+        /// </summary>
+        /// <param name="n">The number of digits in length</param>
+        /// <returns>The largest number, given the length of n-digits</returns>
+        static int FindLargestNumber(int n)
+        {
+            return Convert.ToInt32(Math.Pow(10, n) - 1);
+        }
+    }
+}
