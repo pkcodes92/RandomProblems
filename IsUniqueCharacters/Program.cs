fb73c7c077f6cdcb8cb006beac8000cb7034fdfd
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-11-05 13:42:24 -0500

    Update IsUniqueCharacters/Program.cs
    Adding the necessary documentation

--- IsUniqueCharacters/Program.cs
+++ IsUniqueCharacters/Program.cs
@@ -21,6 +21,12 @@ namespace IsUniqueCharacters
             Console.ReadKey();
         }
 
+        /// <summary>
+        /// Looks at the characters in the provided string to see if the entire string
+        /// contains unique characters
+        /// </summary>
+        /// <param name="inputString">The string that was provided by the user</param>
+        /// <returns>A boolean value to determine if the string contains unique characters or not</returns>
         private static bool ContainsUniqueChars(string inputString)
         {
             if (inputString.Length > 128)
@@ -28,6 +34,7 @@ namespace IsUniqueCharacters
                 return false;
             }
 
+            // Having the charSet array to be 128 for ASCII encoding
             bool[] charSet = new bool[128];
             for (int i = 0; i < inputString.Length; i++)
             {
