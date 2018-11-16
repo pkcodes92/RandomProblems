874bc3f39e84b85e82f5c578cd158908f17844f2
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-11-16 17:05:24 -0500

    Adding the necessary problems and solutions from w3resource

---
+++ AbsoluteDifference/Program.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace AbsoluteDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a value for n: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FindDifference(n));
+
+            Console.WriteLine("Program has finished execution, please press any key to exit");
+            Console.ReadKey(); 
+        }
+
+        /// <summary>
+        /// Method that will calculate the absolute difference btween n and 51. 
+        /// If n is greater than 51 return triple the absolute difference
+        /// </summary>
+        /// <param name="n">Value passed through the console</param>
+        /// <returns>Absolute difference</returns>
+        private static int FindDifference(int n)
+        {
+            const int x = 51;
+
+            if (n > x)
+            {
+                return (n - x) * 3;
+            }
+            else
+            {
+                return x - n;
+            }
+        }
+    }
+}
--- VariousOperations/Program.cs
+++ VariousOperations/Program.cs
@@ -24,27 +24,56 @@ namespace VariousOperations
             Console.ReadKey(); 
         }
 
+        /// <summary>
+        /// Adds the two numbers
+        /// </summary>
+        /// <param name="x">The first input</param>
+        /// <param name="y">The second input</param>
+        /// <returns>Sum</returns>
         private static int GetSum(int x, int y)
         {
             return x + y; 
         }
 
+        /// <summary>
+        /// Subtracts the two numbers
+        /// </summary>
+        /// <param name="x">The first input</param>
+        /// <param name="y">The second input</param>
+        /// <returns>Difference</returns>
         private static int GetDiff(int x, int y)
         {
             return x - y; 
         }
 
+        /// <summary>
+        /// Multiplies the two numbers
+        /// </summary>
+        /// <param name="x">The first input</param>
+        /// <param name="y">The second input</param>
+        /// <returns>Product</returns>
         private static int GetProduct(int x, int y)
         {
             return x * y;
         }
-
+        /// <summary>
+        /// Divides the two numbers
+        /// </summary>
+        /// <param name="x">The first input</param>
+        /// <param name="y">The second input</param>
+        /// <returns>Quotient</returns>
         private static int GetQuotient(int x, int y)
         {
             var quot = Convert.ToDouble(x) / y;
             return (int)quot; 
         }
 
+        /// <summary>
+        /// Computes the remainder between the two numbers
+        /// </summary>
+        /// <param name="x">The first input</param>
+        /// <param name="y">The second input</param>
+        /// <returns>Remainder</returns>
         private static int GetRemainder(int x, int y)
         {
             return x % y; 
