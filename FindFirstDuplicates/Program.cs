8a6d172bbd921f4337f38ad8dfbbf2af1f9dc182
Pranav Krishnamurthy <pkrish19@outlook.com>
2018-11-18 22:15:48 -0500

    Adding the FindDuplicates code signal problem

---
+++ FindFirstDuplicates/Program.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+
+namespace FindFirstDuplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = new int[] { 2, 1, 3, 5, 3, 2 };
+
+            var result = FirstDuplicates(inputArray);
+
+            Console.WriteLine($"The first duplicates in the input array is found at {result}");
+
+            Console.WriteLine("The program has finished executing, now you may press any key to exit");
+
+            Console.ReadKey();
+        }
+
+        private static int FirstDuplicates(int[] a)
+        {
+            Dictionary<int, int> x = new Dictionary<int, int>();
+            foreach (int item in a)
+            {
+                if (x.ContainsKey(item))
+                {
+                    return item;
+                }
+                else
+                {
+                    x.Add(item, 1); 
+                }
+            }
+
+            return -1;
+        }
+    }
+}
