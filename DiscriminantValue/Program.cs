cc19385023d265c2fb583edc25de60ea5d142185
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-01-05 18:09:33 -0800

    Update Program.cs
    Implemented the full functionality of calculating the discriminant values

--- DiscriminantValue/Program.cs
+++ DiscriminantValue/Program.cs
@@ -1,15 +1,43 @@
 ﻿namespace DiscriminantValue
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a value for A = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a value for B = ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter a value for C = ");
+            int c = int.Parse(Console.ReadLine());
+
+            int discriminant = CalculateDiscriminant(a, b, c);
+
+            if (discriminant > 0)
+            {
+                Console.WriteLine($"Discriminant = {discriminant} and there are 2 solutions");
+            }
+            else if (discriminant == 0)
+            {
+                Console.WriteLine($"Discriminant = {discriminant} and there is only 1 solution");
+            }
+            else
+            {
+                Console.WriteLine($"Discriminant = {discriminant} and there are no real solutions");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static int CalculateDiscriminant(int a, int b, int c)
+        {
+            int discriminant = (int)Math.Pow(b, 2) - (4 * a * c);
+
+            return discriminant;
         }
     }
 }
