3cd2f64fd479250bc1eed6978540c870a41a7a0e
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-01-18 09:13:24 -0800

    Adding the problem for finding the k closest elements to a given value

--- LinqInnerJoin/Program.cs
+++ LinqInnerJoin/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace LinqInnerJoin
 {
     using System.Collections.Generic;
+    using System;
+    using System.Linq;
 
     public class Program
     {
@@ -25,6 +27,52 @@
                 new Purchase { InvNo = 105, ItemId = 4,  PurQty = 650 },
                 new Purchase { InvNo = 106, ItemId = 1,  PurQty = 458 }
             };
+
+            Console.Write("\nLINQ : Generate an Inner Join between two data sets : ");
+            Console.Write("\n--------------------------------------------------\n");
+            Console.Write("Here is the Item_mast List : ");
+            Console.Write("\n-------------------------\n");
+
+            foreach (var item in itemList)
+            {
+                Console.WriteLine(
+                "Item Id: {0}, Description: {1}",
+                item.ItemId,
+                item.ItemDesc);
+            }
+
+            Console.Write("\nHere is the Purchase List : ");
+            Console.Write("\n--------------------------\n");
+
+            foreach (var item in purchList)
+            {
+                Console.WriteLine(
+                "Invoice No: {0}, Item Id : {1},  Quantity : {2}",
+                item.InvNo,
+                item.ItemId,
+                item.PurQty);
+            }
+
+            Console.Write("\nHere is the list after joining  : \n\n");
+
+            var innerJoin = from e in itemList
+                            join d in purchList
+                            on e.ItemId equals d.ItemId
+                            select new
+                            {
+                                itId = e.ItemId, 
+                                itDesc = e.ItemDesc,
+                                prQty = d.PurQty
+                            };
+
+            Console.WriteLine("Item ID\t\tItem Name\tPurchase Quantity");
+            Console.WriteLine("-------------------------------------------------------");
+            foreach (var data in innerJoin)
+            {
+                Console.WriteLine(data.itId + "\t\t" + data.itDesc + "\t\t" + data.prQty);
+            }
+
+            Console.ReadKey();
         }
     }
 
