66b3071184b6f0dcce4a9c7164551bc4966ec0a2
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-03-05 14:28:59 -0800

    More changes being made
    Adding things about structs and making sure to have sample problems

---
+++ StructBasicsComplexSample/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace StructBasicsComplexSample
+{
+    using System;
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ StructBasicsProblem1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace StructBasicsProblem1
+{
+    using System;
+
+    struct ValuesStruct
+    {
+        public int x;
+        public int y; 
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\n Declaring a simple structure: \n");
+            Console.Write("-----------------------------------\n");
+
+            ValuesStruct primaryStruct = new ValuesStruct
+            {
+                x = int.Parse(Console.ReadLine()), 
+                y = int.Parse(Console.ReadLine())
+            };
+
+            int sum = primaryStruct.x + primaryStruct.y;
+            Console.WriteLine($"The sum of {primaryStruct.x} and {primaryStruct.y} is {sum}");
+
+            Console.WriteLine("Execution of this program has completed. You may now press any key to exit");
+            Console.ReadKey(); 
+        }
+    }
+}
