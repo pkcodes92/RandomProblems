bb110a4c38468e3616338c3d508fc6c7a325d1cc
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-03-08 12:17:04 -0800

    Adding the SortedSearch problem
    Adding the problem where we find the number of elements in an array that are less than a specific value

---
+++ SortedSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SortedSearch
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CountNumbers(new int[] { 1, 3, 5, 6, 7 }, 4));
+            Console.WriteLine("The program has finished execution - you can press any key to exit");
+            Console.ReadKey();
+        }
+
+        public static int CountNumbers(int[] sortedArray, int lessThan)
+        {
+            var validCnt = 0;
+
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                if (sortedArray[i] < lessThan)
+                {
+                    validCnt++;
+                }
+            }
+
+            return validCnt;
+        }
+    }
+}
