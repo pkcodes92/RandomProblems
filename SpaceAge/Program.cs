d6006ec2f121ed59b535eb4bc8ed44b7472dd827
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-03-28 10:35:27 -0700

    Having the problem to calculate someone's age on various planets given their age in seconds

---
+++ SpaceAge/Program.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// Given an age in seconds, calculate how old someone would be on: 
+/// Earth: orbital period 365.25 Earth days, or 31557600 seconds
+/// Mercury: orbital period 0.2408467 Earth years
+/// Venus: orbital period 0.61519726 Earth years
+/// Mars: orbital period 1.8808158 Earth years
+/// Jupiter: orbital period 11.862615 Earth years
+/// Saturn: orbital period 29.447498 Earth years
+/// Uranus: orbital period 84.016846 Earth years
+/// Neptune: orbital period 164.79132 Earth years
+/// </summary>
+namespace SpaceAge
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SpaceAge/SpaceCalculations.cs
@@ -0,0 +1,59 @@
+﻿namespace SpaceAge
+{
+    public class SpaceCalculations
+    {
+        public struct Constants
+        {
+            /// <summary>
+            /// The number of seconds in a year on Earth
+            /// </summary>
+            public const double SECONDS_IN_EARTH_YEAR = 31557600.0;
+
+            /// <summary>
+            /// The orbital period of Mercury as measured in Earth years
+            /// </summary>
+            public const double MERCURY_PERIOD = 0.2408467;
+
+            /// <summary>
+            /// The orbital period of Venus as measured in Earth years
+            /// </summary>
+            public const double VENUS_PERIOD = 0.61519726;
+
+            /// <summary>
+            /// The orbital period of Mars as measured in Earth years
+            /// </summary>
+            public const double MARS_PERIOD = 1.8808158;
+
+            /// <summary>
+            /// The orbital period of Jupiter as measured in Earth years
+            /// </summary>
+            public const double JUPITER_PERIOD = 11.862615;
+
+            /// <summary>
+            /// The orbital period of Saturn as measured in Earth years
+            /// </summary>
+            public const double SATURN_PERIOD = 29.447498;
+
+            /// <summary>
+            /// The orbital period of Uranus as measured in Earth years
+            /// </summary>
+            public const double URANUS_PERIOD = 84.016846;
+
+            /// <summary>
+            /// The orbital period of Neptune as measured in Earth years
+            /// </summary>
+            public const double NEPTUNE_PERIOD = 164.79132; 
+        }
+
+        private readonly int Seconds;
+        public SpaceCalculations(int seconds)
+        {
+            Seconds = seconds;
+        }
+
+        public double OnEarth()
+        {
+            return Seconds / Constants.SECONDS_IN_EARTH_YEAR; 
+        }
+    }
+}
