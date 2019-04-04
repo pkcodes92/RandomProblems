de3261f49ed11e36b7a2b07b3339a217a9e29903
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-04-04 13:58:18 -0700

    Latest changes

---
+++ TowersOfHanoi/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace TowersOfHanoiApp
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TowerOfHanoi T = new TowerOfHanoi();
+            string cNumDiscs;
+            Console.Write("Enter the number of discs: ");
+            cNumDiscs = Console.ReadLine();
+            T.NumDiscs = Convert.ToInt32(cNumDiscs);
+            T.MoveTower(T.NumDiscs, 1, 3, 2);
+            Console.WriteLine("The program execution has completed - you may press any key to exit");
+            Console.ReadKey(); 
+        }
+    }
+}
---
+++ TowersOfHanoi/TowerOfHanoi.cs
@@ -0,0 +1,41 @@
+﻿namespace TowersOfHanoiApp
+{
+    using System;
+
+    public class TowerOfHanoi
+    {
+        int m_numDiscs;
+
+        public TowerOfHanoi()
+        {
+            NumDiscs = 0; 
+        }
+
+        public TowerOfHanoi(int newVal)
+        {
+            NumDiscs = newVal;
+        }
+
+        public int NumDiscs
+        {
+            get { return m_numDiscs; }
+            set
+            {
+                if (value > 0)
+                {
+                    m_numDiscs = value;
+                }
+            }
+        }
+
+        public void MoveTower(int n, int from, int to, int other)
+        {
+            if (n > 0)
+            {
+                MoveTower(n - 1, from, other, to);
+                Console.WriteLine("Move disk {0} from tower {1} to tower {2}", n, from, to);
+                MoveTower(n - 1, other, to, from); 
+            }
+        }
+    }
+}
