4fdc7c894aa002d08fbb62fdb97fb0eef1bb1942
Pranav Krishnamurthy <pkrish19@outlook.com>
2019-05-09 16:37:38 -0700

    Ensuring to have the code completed for the randomproblems solution

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -1,16 +1,49 @@
 ﻿namespace NumberGuessingGame
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
 
     class Program
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                int randomNumber = NewNumber(1, 101);
+                int count = 1;
+                while (true)
+                {
+                    Console.Write("Enter a number between 1 and 100 (0 will quit)");
+                    int guessInt = int.Parse(Console.ReadLine());
+                    if (guessInt == 0)
+                    {
+                        return;
+                    }
+                    else if (guessInt < randomNumber)
+                    {
+                        Console.WriteLine("Low, try again!");
+                        ++count;
+                        continue;
+                    }
+                    else if (guessInt > randomNumber)
+                    {
+                        Console.WriteLine("High, try again!");
+                        ++count;
+                        continue;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Bingo! The number I thought about is {0}", randomNumber);
+                        Console.WriteLine("It took you {0} {1}. \n", count, count == 1 ? "try" : "tries");
+                        break;
+                    }
+                }
+            }
+        }
+
+        static int NewNumber(int min, int max)
+        {
+            Random randomGen = new Random();
+            return randomGen.Next(min, max);
         }
     }
 }
