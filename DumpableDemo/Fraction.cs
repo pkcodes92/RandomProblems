5e0970d97a02c6e004a65351f82d157cefe60760
Pranav Krishnamurthy <pkrish19@outlook.com>
2020-07-16 14:44:10 -0400

    Having things properly updated

--- DumpableDemo/Fraction.cs
+++ DumpableDemo/Fraction.cs
@@ -2,18 +2,35 @@
 {
     using System;
 
+    /// <summary>
+    /// This is the fraction class.
+    /// </summary>
     public class Fraction : IDumpable
     {
+        /// <summary>
+        /// Public integers.
+        /// </summary>
         public int z, n;
         
+        /// <summary>
+        /// The default constructor.
+        /// </summary>
+        /// <param name="a">A random integer value.</param>
+        /// <param name="b">Another random integer value.</param>
         public Fraction(int a, int b)
         {
             z = a;
             n = b; 
         }
 
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// This method writes to the console.
+        /// </summary>
         public void Dump()
         {
             Console.WriteLine("Fraction : " + z + "/" + n); 
--- DynamicPartitions/OrderableListPartitioner.cs
+++ DynamicPartitions/OrderableListPartitioner.cs
@@ -17,17 +17,28 @@ namespace DynamicPartitions
     {
         private readonly IList<TSource> m_input; 
 
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
+        /// <param name="input">The input list.</param>
         public OrderableListPartitioner(IList<TSource> input): base(true, false, true)
         {
             m_input = input;
         }
 
-        // Must override to return true.
+        /// <summary>
+        /// Must override to return true.
+        /// </summary>
         public override bool SupportsDynamicPartitions
         {
             get { return true; }
         }
 
+        /// <summary>
+        /// Gets the orderable partitions.
+        /// </summary>
+        /// <param name="partitionCount">An integer variable.</param>
+        /// <returns>A list of key value pairs.</returns>
         public override IList<IEnumerator<KeyValuePair<long, TSource>>> GetOrderablePartitions(int partitionCount)
         {
             var dynamicPartitions = GetOrderableDynamicPartitions();
@@ -41,6 +52,10 @@ namespace DynamicPartitions
             return partitions;
         }
 
+        /// <summary>
+        /// Method that will get the orderable dynamic partitions.
+        /// </summary>
+        /// <returns></returns>
         public override IEnumerable<KeyValuePair<long, TSource>> GetOrderableDynamicPartitions()
         {
             return new ListDynamicPartitions(m_input); 
