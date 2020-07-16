ea17c3a4d0bb48db7320e1193abb309fd15d7f74
coderkrishna92 <pkrish19@outlook.com>
2020-07-15 23:42:55 -0400

    Having things properly moving forward

--- AbsoluteDifference/Program.cs
+++ AbsoluteDifference/Program.cs
@@ -1,18 +1,30 @@
+// <copyright file="Program.cs" company="TCS Ltd">
+// Copyright (c) TCS Ltd. All rights reserved.
+// </copyright>
+
 namespace AbsoluteDifference
 {
     using System;
+    using System.Globalization;
 
-    class Program
+    /// <summary>
+    /// The main class for this project.
+    /// </summary>
+    public static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// This is the main driver method.
+        /// </summary>
+        /// <param name="args">Project specific arguments.</param>
+        public static void Main(string[] args)
         {
             Console.WriteLine("Please provide a value for n: ");
-            int n = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
             Console.WriteLine(FindDifference(n));
 
             Console.WriteLine("Program has finished execution, please press any key to exit");
-            Console.ReadKey(); 
+            Console.ReadKey();
         }
 
         /// <summary>
