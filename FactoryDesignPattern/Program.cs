193980d038b6da735ddf330a3af86d99b36565bb
Pranav Krishnamurthy <pkrish19@outlook.com>
2020-07-23 12:22:40 -0400

    Having things being done correctly

--- FactoryDesignPattern/Program.cs
+++ FactoryDesignPattern/Program.cs
@@ -5,10 +5,6 @@
 namespace FactoryDesignPattern
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     /// <summary>
     /// This is the main driver class.
@@ -21,6 +17,29 @@ namespace FactoryDesignPattern
         /// <param name="args">Project specific, command line arguments.</param>
         public static void Main(string[] args)
         {
+            CardFactory factory = null;
+            Console.WriteLine("Enter the card type you would like to use: ");
+            string car = Console.ReadLine();
+
+            switch (car.ToLower())
+            {
+                case "moneyback":
+                    factory = new MoneyBackFactory(50000, 0);
+                    break;
+                case "titanium":
+                    factory = new TitaniumFactory(100000, 500);
+                    break;
+                case "platinum":
+                    factory = new PlatinumFactory(500000, 1000);
+                    break;
+                default:
+                    break;
+            }
+
+            CreditCard creditCard = factory.GetCreditCard();
+            Console.WriteLine("\nYour card details are below : \n");
+            Console.WriteLine($"Card Type: {creditCard.CardType}\nCredit Limit: {creditCard.CreditLimit}\nAnnual Charge: {creditCard.AnnualCharge}");
+            Console.ReadKey();
         }
     }
 }
