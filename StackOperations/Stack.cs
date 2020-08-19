7574d3bfd5ea608b6b9bab374346cd685b53441c
Pranav Krishnamurthy <pkrish19@outlook.com>
2020-08-19 11:48:13 -0400

    Having things done properly for the code quality

--- StackOperations/Stack.cs
+++ StackOperations/Stack.cs
@@ -1,13 +1,31 @@
-﻿namespace StackOperations
+﻿// <copyright file="Stack.cs" company="TCS Ltd">
+// Copyright (c) TCS Ltd. All rights reserved.
+// </copyright>
+
+namespace StackOperations
 {
     using System;
 
+    /// <summary>
+    /// This class implements the methods defined in <see cref="IStackADT"/>.
+    /// </summary>
     public class Stack : IStackADT
     {
+        /// <summary>
+        /// Gets or sets the stack size set.
+        /// </summary>
         public int StackSizeSet { get; set; }
+
+        /// <summary>
+        /// The top most element.
+        /// </summary>
         public int top;
+
         readonly object[] item;
 
+        /// <summary>
+        /// This is the default constructor.
+        /// </summary>
         public Stack()
         {
             StackSizeSet = 10;
@@ -15,6 +33,10 @@
             top = -1;
         }
 
+        /// <summary>
+        /// Initializes an instance of the stack.
+        /// </summary>
+        /// <param name="capacity">The stack capacity.</param>
         public Stack(int capacity)
         {
             StackSizeSet = capacity;
@@ -22,12 +44,20 @@
             top = -1;
         }
 
+        /// <summary>
+        /// This method checks for an empty stack.
+        /// </summary>
+        /// <returns>Returns a value to indicate whether or not the stack is empty.</returns>
         public bool IsEmpty()
         {
             if (top == -1) return true;
             return false;
         }
 
+        /// <summary>
+        /// This method pushes on to the stack.
+        /// </summary>
+        /// <param name="element">The element to add to the stack.</param>
         public void Push(object element)
         {
             if (top == (StackSizeSet - 1))
@@ -41,6 +71,10 @@
             }
         }
 
+        /// <summary>
+        /// Removes an element of the stack.
+        /// </summary>
+        /// <returns>The object that is removed from the stack.</returns>
         public object Pop()
         {
             if (IsEmpty())
@@ -54,6 +88,10 @@
             }
         }
 
+        /// <summary>
+        /// This method looks at the top most element of the stack.
+        /// </summary>
+        /// <returns>The top most element.</returns>
         public object Peek()
         {
             if (IsEmpty())
@@ -67,6 +105,9 @@
             }
         }
 
+        /// <summary>
+        /// This method displays all of the elements that are on the stack currently.
+        /// </summary>
         public void Display()
         {
             for (int i = top; i > -1; i--)
