9e89ae80b6fb9896d67f4c037505be50b03b00db
pkcodes92 <pkrish19@outlook.com>
2020-12-19 15:10:41 -0500

    Making sure to have all of the problems being consolidated properly

---
+++ MasterOfAllProblems/ProblemSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+
+namespace MasterOfAllProblems
+{
+    public class ProblemSolver
+    {
+        public static void AbsoluteDifference()
+        {
+            Console.WriteLine("Please provide a value for n: ");
+            int n = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(FindDifference(n));
+        }
+
+        /// <summary>
+        /// Method that will calculate the absolute difference btween n and 51. 
+        /// If n is greater than 51 return triple the absolute difference.
+        /// </summary>
+        /// <param name="n">Value passed through the console.</param>
+        /// <returns>Absolute difference.</returns>
+        private static int FindDifference(int n)
+        {
+            const int x = 51;
+
+            if (n > x)
+            {
+                return (n - x) * 3;
+            }
+            else
+            {
+                return x - n;
+            }
+        }
+    }
+}
---
+++ MasterOfAllProblems/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace MasterOfAllProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to the RandomProblems app!");
+            Console.WriteLine("Please enter a problem you want to solve: ");
+            var problemChoice = Console.ReadLine();
+
+            switch (problemChoice)
+            {
+                case "AbsoluteDifference":
+                    ProblemSolver.AbsoluteDifference();
+                    break;
+                case "AcronymMaker":
+                    ProblemSolver.AcronymMaker();
+                    break;
+                default:
+                    Console.WriteLine($"Sorry I don't think that I have a sample of your problem: {problemChoice}");
+                    break;
+            }
+
+            Console.WriteLine("Execution has completed, please press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
