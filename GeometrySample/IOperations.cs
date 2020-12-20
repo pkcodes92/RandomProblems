4cb01fa1cf3b6edbf7b5bfd857009a172794827f
Pranav Krishnamurthy <pkrish19@outlook.com>
2020-12-19 19:33:42 -0500

    Removing all of the projects accordingly. Will be getting the code from the master branch through copy paste

--- GeometrySample/IOperations.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GeometrySample
-{
-    public interface IOperations
-    {
-        double calculateArea(double width, double height);
-
-        double calculatePerim(double width, double height);
-    }
-}
